83be6fb48ce356d709f4ca69fa0cd7e7c1c3854b
Kristian Heruc <kristian@heruc.com>
2024-07-30 14:33:42 +0200

    feat: add some sample data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+export const dynamic = 'force-dynamic'
+
+const getData = () => {
+  const version = process.env.APP_VERSION
+
+  return version
+}
+
 export default function Home() {
+  const version = getData()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Test
+      <strong>{process.env.NEXT_PUBLIC_APP_NAME} v{version} running in {process.env.NEXT_PUBLIC_APP_ENV} environment</strong>
     </main>
   );
 }
